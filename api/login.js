47cb98a1a79a0c52f0e4284da488e19be23f7967
Apoorv Darshan <ad13dtu@gmail.com>
2025-07-15 08:11:53 +0530

    fixed vercel errror

--- api/login.js
+++ api/login.js
@@ -1,6 +1,6 @@
 const { findUser, updateUserLogin } = require('../lib/mongodb-vercel');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // Enable CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
